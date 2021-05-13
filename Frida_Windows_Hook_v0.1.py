4c19e26e2ab4322d26f4b4b831fe346828e19d38
nam3z1p <38063224+nam3z1p@users.noreply.github.com>
2021-05-13 22:27:10 +0900

    Update Frida_Windows_Hook_v0.1.py

--- Frida_Windows_Hook_v0.1.py
+++ Frida_Windows_Hook_v0.1.py
@@ -35,80 +35,80 @@ console.log('[*] '+target_library+' baseAddr : ' + baseAddr);
 console.log('[*] '+target_API+' hookAddr : ' + hookAddr);
 
 Interceptor.attach(hookAddr, {
-        //Function is start
-        onEnter: function (args) {
-            console.log('##########################################################');
-            console.log("[+] Send Called from:\\n" + Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join("\\n") + " ");
-            console.log('##########################################################');
-            console.log('[+] Called '+target_API+'!! [' + hookAddr + ']');
-            console.log('[+] args[0] = [' + args[0] + ']');
-            //DumpAddr('args[0]', args[0], 0x20);
-            console.log('[+] args[1] = [' + args[1] + ']');
-            DumpAddr('args[1]', args[1], 0x40);
-            console.log('[+] args[2] = [' + args[2] + ']');
-            //DumpAddr('args[2]', args[2], 0x30);
-            //WriteMemory('args[2]', args[2], 0x120);
-        },
-        //Function is finished
-        onLeave: function (retval) {
-            console.log('##########################################################');
-            console.log('[+] (Origin) typeof(retval) : ' + typeof(retval));
-            console.log('[+] (Origin) retval : ' + retval);
-
-            // Return retval change
-            /*
-            retval.replace(0);
-            console.log('[+] (forgery) typeof(retval) : ' + typeof(retval));
-            console.log('[+] (forgery) retval : ' + retval);
-            */
-
-            // return eax change */
-            //this.context.eax = 0x0;
-
-            console.log('Context information:');
-            console.log('Context  : ' + JSON.stringify(this.context));
-            console.log('ReturnAddr : ' + this.returnAddress);
-            console.log('ThreadId : ' + this.threadId);
-            console.log('Depth    : ' + this.depth);
-            console.log('Errornr  : ' + this.err);
-            console.log('##########################################################');
-        }
-    });
-
-    //Print out data array
-    function DumpAddr(info, addr, size) {
-        if (addr.isNull())
-            return;
-        console.log('Data dump ' + info + ' :');
-        var buf = Memory.readByteArray(addr, size);
-        // If you want color magic, set ansi to true
-        console.log(hexdump(buf, { offset: 0, length: size, header: true, ansi: false }));
-    }
-
-    //Write memory data
-    function WriteMemory(info, addr, size) {
-        if (addr.isNull())
-            return;
+    //Function is start
+    onEnter: function (args) {
+        console.log('##########################################################');
+        console.log("[+] Send Called from:\\n" + Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join("\\n") + " ");
+        console.log('##########################################################');
+        console.log('[+] Called '+target_API+'!! [' + hookAddr + ']');
+        console.log('[+] args[0] = [' + args[0] + ']');
+        //DumpAddr('args[0]', args[0], 0x20);
+        console.log('[+] args[1] = [' + args[1] + ']');
+        DumpAddr('args[1]', args[1], 0x40);
+        console.log('[+] args[2] = [' + args[2] + ']');
+        //DumpAddr('args[2]', args[2], 0x30);
+        //WriteMemory('args[2]', args[2], 0x120);
+    },
+    //Function is finished
+    onLeave: function (retval) {
+        console.log('##########################################################');
+        console.log('[+] (Origin) typeof(retval) : ' + typeof(retval));
+        console.log('[+] (Origin) retval : ' + retval);
         
-        console.log('Write Memory ' + info + ' :');
-        
-        for (var i=0x00; i<size; i++){
-            Memory.protect(addr.add(i), 0x01, "rwx");
-            Memory.writeByteArray(addr.add(i), [0x65]);
-            //point.push(addr.add(i));
-            console.log(addr.add(i));
-        }
-    }
+        // Return retval change
+        /*
+        retval.replace(0);
+        console.log('[+] (forgery) typeof(retval) : ' + typeof(retval));
+        console.log('[+] (forgery) retval : ' + retval);
+        */
 
-    function ResolveAddr(addr) {
-        // Enter the base address of dll as seen in your favorite disassembler (here IDA)
-        // Calculate offset in memory from base address in IDA database
-        var idaBase = ptr('0x7FF60000');
-        var offset = ptr(addr).sub(idaBase);
-        var result = baseAddr.add(offset);
-        console.log('[+] New addr=' + result);
-        return result;
+        // return eax change */
+        //this.context.eax = 0x0;
+
+        console.log('Context information:');
+        console.log('Context  : ' + JSON.stringify(this.context));
+        console.log('ReturnAddr : ' + this.returnAddress);
+        console.log('ThreadId : ' + this.threadId);
+        console.log('Depth    : ' + this.depth);
+        console.log('Errornr  : ' + this.err);
+        console.log('##########################################################');
     }
+});
+
+//Print out data array
+function DumpAddr(info, addr, size) {
+    if (addr.isNull())
+        return;
+    console.log('Data dump ' + info + ' :');
+    var buf = Memory.readByteArray(addr, size);
+    // If you want color magic, set ansi to true
+    console.log(hexdump(buf, { offset: 0, length: size, header: true, ansi: false }));
+}
+
+//Write memory data
+function WriteMemory(info, addr, size) {
+    if (addr.isNull())
+        return;
+
+    console.log('Write Memory ' + info + ' :');
+
+    for (var i=0x00; i<size; i++){
+        Memory.protect(addr.add(i), 0x01, "rwx");
+        Memory.writeByteArray(addr.add(i), [0x65]);
+        //point.push(addr.add(i));
+        console.log(addr.add(i));
+    }
+}
+
+function ResolveAddr(addr) {
+    // Enter the base address of dll as seen in your favorite disassembler (here IDA)
+    // Calculate offset in memory from base address in IDA database
+    var idaBase = ptr('0x7FF60000');
+    var offset = ptr(addr).sub(idaBase);
+    var result = baseAddr.add(offset);
+    console.log('[+] New addr=' + result);
+    return result;
+}
 
 """)
 script.on("message", on_message)
